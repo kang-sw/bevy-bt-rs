8ba2324d943ac3cf1afd6e3f220ee949470bb726
kang-sw <ki6080@gmail.com>
2023-10-18 00:17:00 +0900

    core: add more data types

--- core/src/graph.rs
+++ core/src/graph.rs
@@ -8,22 +8,54 @@ use std::collections::HashMap;
 use indexmap::IndexMap;
 
 /// BevyBT project structure. The whole directory structure is loaded into this struct.
+///
+/// - TODO: Workspace directory manipulation functionalities
+///     - `write_tree`: Validates tree implementation and write tree and corresponding tasks to file
+///       system
+///         - Validates if tree's every [`TreeRef`] points valid tree path.
+///         - Validates if there's no conflicting [`UserAction`] definitions.
+///     - `reload`: Reload workspace from file system
 pub struct Workspace {
     trees: HashMap<String, BehaviorTree>,
-    task_defs: HashMap<String, TaskDefinition>,
+    user_actions: HashMap<String, UserAction>,
+    user_structs: HashMap<String, UserStruct>,
 }
 
 pub struct BehaviorTree {
+    /// The root node, which is entry point of execution hierarchy.
     root: Node,
+
+    /// Floating nodes that does not belong to any parent node.
+    floating_nodes: Vec<Node>,
+
+    /// Blackboard variable definitions
     blackboard: HashMap<String, Argument>,
 }
 
-pub enum Node {
+pub struct Node {
+    /// Overrided title of this node.
+    pub title: String,
+
+    /// Decorator of this node
+    pub decorators: Vec<Decorator>,
+
+    /// Position in grid
+    pub position: [f64; 2],
+
+    /// Comment of this node
+    pub comment: String,
+
+    /// Actual node class
+    pub class: NodeClass,
+}
+
+pub enum NodeClass {
     Sequence(SequenceNode),
     Selector(SelectorNode),
+    Decorator(Decorator),
 
     /// Instantiation of single action
-    Task(TaskDefinition),
+    Task(Task),
 
     /// Reference to another tree in relative file system.
     TreeRef(TreeRef),
@@ -46,20 +78,32 @@ pub enum Argument {
     F32(f32),
     F64(f64),
     String(String),
-    FixedArray(Vec<Argument>),
+    FixedArray(Box<Argument>, usize),
+    Tuple(Vec<Argument>),
     DynamicArray(ArgumentDiscriminants, Option<usize>),
 
     /// Will be generated as [`Option<Box<dyn std::any::Any + Send>>`]
     BoxedAny,
+
+    /// Handle to Bevy entity
+    Entity,
+
+    /// Path to user defined struct
+    UserStruct(String),
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct UserStruct {
+    pub fields: IndexMap<String, Argument>,
 }
 
 pub struct SequenceNode {
-    children: Vec<Node>,
+    pub children: Vec<Node>,
 }
 
 pub struct SelectorNode {
-    random: bool,
-    children: Vec<Node>,
+    pub random: bool,
+    pub children: Vec<Node>,
 }
 
 pub struct TaskNode {}
@@ -72,21 +116,40 @@ pub struct TreeRef {
 pub struct Parallel {
     /// Only result of the main node is evaluated. Once main node is completed, all other parallel
     /// nodes are aborted
-    pub main: Node,
+    pub main: NodeClass,
 
     /// Every node runs in parallel
-    pub subs: Vec<Node>,
+    pub subs: Vec<NodeClass>,
 }
 
-pub struct Task {
-    pub definition: TaskDefinition,
+pub enum Task {
+    UserAction(UserAction),
+
+    WaitFor {
+        timeout: f64,
+    },
+
+    /// Wait forever before any interrupt
+    WaitForever,
 }
 
-/// Action definition.
-pub struct TaskDefinition {
-    /// Input blackboard arguments and their types
-    pub inputs: IndexMap<String, Argument>,
+pub struct UserAction {
+    inputs: Vec<Argument>,
+    outputs: Vec<Argument>,
+}
 
-    /// Output blackboard arguments and their types
-    pub outputs: IndexMap<String, Argument>,
+pub enum Decorator {
+    /// TODO: Blackboard evaluation expression
+
+    /// Check if blackboard argument of given name equals with given value. Type must be same.
+    BlackboardEquals(String, Argument),
+
+    /// For boolean, non-false. For numerics, non-zero. For strings or references, non-empty.
+    BlackboardNonEmpty(String),
+
+    /// Expects user returns true or false; if user action executer returns [`None`], it'll
+    /// use specified boolean value as default.
+    ///
+    /// This differs from plain [`UserAction`] task, which is
+    UserEval { logic_name: String, inputs: IndexMap<String, Argument>, default: bool },
 }
